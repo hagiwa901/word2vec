84dfc609b9c9fa6c4e4d32e90008cee896f04762
hagiwa901 <kedama4156991@gmail.com>
2023-08-12 22:46:34 +0900

    add train

--- src/word2vec/__init__.py
+++ src/word2vec/__init__.py
@@ -1,2 +1,17 @@
-def hello():
+import logging
+import gensim
+from gensim.models import Word2Vec
+
+logging.basicConfig(level=logging.INFO)
+
+def hello() -> None:
     return "Hello from word2vec!"
+
+def train(text_file:str, model_path: str) -> None:
+    logging.debug("train!!")
+    with open(text_file) as f:
+        sentences = [s.rstrip().split() for s in f.readlines()]
+    logging.debug(sentences)
+    model = Word2Vec(sentences,min_count=1)
+    logging.debug(model_path)
+    model.save(model_path)
