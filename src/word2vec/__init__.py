a68c47c98b93c00888836080eb35be36ea216c3f
hagiwa901 <kedama4156991@gmail.com>
2023-08-12 23:01:09 +0900

    add predict

--- src/word2vec/__init__.py
+++ src/word2vec/__init__.py
@@ -4,14 +4,16 @@ from gensim.models import Word2Vec
 
 logging.basicConfig(level=logging.INFO)
 
-def hello() -> None:
-    return "Hello from word2vec!"
-
 def train(text_file:str, model_path: str) -> None:
-    logging.debug("train!!")
+    logging.info("train!!")
     with open(text_file) as f:
         sentences = [s.rstrip().split() for s in f.readlines()]
-    logging.debug(sentences)
-    model = Word2Vec(sentences,min_count=1)
-    logging.debug(model_path)
+    logging.info(sentences)
+    model = Word2Vec(sentences, min_count=1)
+    logging.info(model_path)
     model.save(model_path)
+
+def predict(model_path: str, word:str) -> None:
+    model = Word2Vec.load(model_path)
+    for item, value in model.wv.most_similar(word):
+        print(item, value)
