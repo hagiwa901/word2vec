0684e9068b7c9858eb76359e0708a79a932a8def
hagiwa901 <kedama4156991@gmail.com>
2023-08-18 23:44:33 +0900

    fix train.py

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 import logging
+import hydra
+from omegaconf import DictConfig, OmegaConf
 import gensim
 from gensim.models import Word2Vec
 
@@ -6,11 +8,17 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def main(text_file: str, model_path: str) -> None:
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
     logging.info("train start")
+    text_file = cfg.config.text_file
+    model_path = cfg.config.model_path
+    logging.info("read_text ")
     with open(text_file) as f:
         sentences = [s.rstrip().split() for s in f.readlines()]
-    model = Word2Vec(sentences, min_count=1)
+    logging.info("read_text done")
+    min_count = cfg.word2vec.min_count
+    model = Word2Vec(sentences, min_count=min_count)
     logging.info("word2vec model path:" + model_path)
     model.save(model_path)
     logging.info("train done")
