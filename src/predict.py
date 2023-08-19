e431989169356f312f27e46fd37559c8fff94698
hagiwa901 <kedama4156991@gmail.com>
2023-08-19 14:11:10 +0900

    fix predict

---
+++ src/predict.py
@@ -0,0 +1,25 @@
+import logging
+from tqdm import tqdm
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import gensim
+from gensim.models import Word2Vec
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    logging.info("predict start")
+    logging.info(OmegaConf.to_yaml(cfg))
+    model_path = cfg.config.model_path
+    word = cfg.predict.similar.word
+    model = Word2Vec.load(model_path)
+    for item, value in model.wv.most_similar(word):
+        print(item, value)
+    logging.info("predict done")
+
+
+if __name__ == '__main__':
+    main()
--- src/word2vec/__init__.py
+++ src/word2vec/__init__.py
@@ -1,11 +1,3 @@
 import logging
-import gensim
-from gensim.models import Word2Vec
-
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-def predict(model_path: str, word:str) -> None:
-    model = Word2Vec.load(model_path)
-    for item, value in model.wv.most_similar(word):
-        print(item, value)
