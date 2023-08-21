d745d42e7721d7845df7fe490ac4c80f9edd7ab5
hagiwa901 <kedama4156991@gmail.com>
2023-08-22 00:14:47 +0900

    fix make_wiki_corpus.py

--- src/make_wiki_corpus.py
+++ src/make_wiki_corpus.py
@@ -1,28 +1,33 @@
 import logging
+import re
+from tqdm import tqdm
+import hydra
+from omegaconf import DictConfig, OmegaConf
 import os
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def main(dir_root_path: str, save_text: str) -> None:
-    logging.info("make corpus")
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    dir_root_path = cfg.config.dir_root_path
+    save_text = cfg.config.save_text
     wiki_dir_list = os.listdir(dir_root_path)
     wiki_text = ''
-    for dir_list in wiki_dir_list:
+    pattern = re.compile(r'[^</*doc.*]|[^\S]')
+    for dir_list in tqdm(wiki_dir_list):
         wiki_dir_path = os.path.join(dir_root_path, dir_list)
         wiki_file_list = os.listdir(wiki_dir_path)
-        for wiki_file in wiki_file_list:
+        for wiki_file in tqdm(wiki_file_list, leave=False):
             wiki_file_path = os.path.join(wiki_dir_path, wiki_file)
             with open(wiki_file_path) as f:
                 sentences = [s.rstrip() for s in f.readlines()]
-            for sentence in sentences:
-                wiki_text += sentence + '\n'
-            logging.info(wiki_file_path + " done")
-    logging.info("save_text_file_name: " + save_text)
+                for sentence in sentences:
+                    if pattern.match(sentence):
+                        wiki_text += sentence + '\n'
     with open(save_text, 'w') as f:
         f.write(wiki_text)
-    logging.info("make corpus done")
 
 
 if __name__ == '__main__':
